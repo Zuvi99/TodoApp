a7c96660867ae05b2b1452f3aa2ae47466080bfb
Adedoyin Adepetun <doyinadepetun@gmail.com>
2024-03-06 13:47:02 +0000

    working on checklist

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,68 @@
-import {Box, Button, FormLabel, HStack, Input, useColorMode, VStack} from "@chakra-ui/react";
+import {
+    Box,
+    Button, Checkbox,
+    FormLabel,
+    Heading,
+    HStack,
+    Input,
+    useColorMode,
+    VStack
+} from "@chakra-ui/react";
+import {useState} from "react";
 
 function App() {
     const { colorMode, toggleColorMode } = useColorMode();
-    return (
-        <Box
-            width={"100%"}
-            margin={"50px"}
-        >
-            <VStack>
-                <HStack width={"100%"} spacing={"70%"}>
-                    <Box>
-                        <FormLabel fontWeight={"bold"}>New Task</FormLabel>
-                        < Input width={"500px"}/>
-                    </Box>
-                    <Box>
-                        <Button
-                            size={"md"}
-                            onClick={toggleColorMode}
-                        >
-                            {colorMode === "light" ? "Dark" : "Light"} Mode
-                        </Button>
-                    </Box>
-                </HStack>
-            </VStack>
+    const [newItem, setNewItem] = useState("")
 
-        </Box>
+    return (
+        <>
+            <Box
+                width={"100%"}
+                margin={"50px"}
+            >
+                <VStack>
+                    <HStack width={"100%"} spacing={"70%"}>
+                        <Box>
+                            <VStack>
+                                <Box>
+                                    <FormLabel fontWeight={"bold"}>New Task</FormLabel>
+                                    < Input width={"500px"}/>
+                                </Box>
+                                <Box width={"100%"}>
+                                    <Button
+                                        // onClick={}
+                                        width={"100%"}
+                                    >Add Task</Button>
+                                </Box>
+                                <Box width={"100%"}>
+                                    <Heading>Tasks</Heading>
+                                </Box>
+                                <Box width={"100%"}>
+                                    <VStack>
+                                        <Box width={"100%"}>
+                                            <Checkbox colorScheme={"green"}>
+                                                Item 1
+                                            </Checkbox>
+                                            <Button marginLeft={"25px"} size={"sm"}>Delete</Button>
+                                        </Box>
+                                    </VStack>
+                                </Box>
+                            </VStack>
+
+                        </Box>
+                        <Box>
+                            <Button
+                                size={"md"}
+                                onClick={toggleColorMode}
+                            >
+                                {colorMode === "light" ? "Dark" : "Light"} Mode
+                            </Button>
+                        </Box>
+                    </HStack>
+                </VStack>
+            </Box>
+        </>
+
     )
 }
 export default App
